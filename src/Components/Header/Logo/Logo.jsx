ed6d25b53803db38760adbfe092a56dd13a86864
allanmiller143 <millerallan17@gmail.com>
2025-05-16 21:50:04 -0300

    logo

--- src/Components/Header/Logo/Logo.jsx
+++ src/Components/Header/Logo/Logo.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useThemeContext } from "../../../Theme/CustomThemeProvider";
 import { Link } from "react-router-dom";
+import LogoDark from "../../../assets/LightLogo.svg";
+import LogoLight from "../../../assets/Logo.svg";
+
 
 const Logo = () => {
   const { mode } = useThemeContext();
@@ -8,7 +11,7 @@ const Logo = () => {
   return (
     <Link to="/" style={{ display: 'inline-block' }}>
       <img
-        src={mode !== 'light' ? "/src/assets/LightLogo.svg" : "/src/assets/Logo.svg"}
+        src={mode !== 'light' ? LogoDark : LogoLight}
         alt="Logo"
         style={{ width: '100px', height: '100px', cursor: 'pointer' }}
       />
